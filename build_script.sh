ca3360405d8d2946acc46a3e95619f6b6e5f8c8e
Guillaume de Cagny <hello@gdc.dev>
2025-06-04 16:15:39 +0200

    add a end2end build script

---
+++ build_script.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+ZINT_DEV_FOLDER=$(readlink -f $(dirname $0))
+
+echo "Zint DEV FOLDER : $ZINT_DEV_FOLDER"
+
+yarn
+cd internal_components/default
+
+yarn
+yarn build
+
+cd $ZINT_DEV_FOLDER
+
+mkdir -p extraResources/components
+cp -r internal_components/default/dist extraResources/components/default
+
+yarn package:applesilicon
+
+cd .webpack
+
+mv ./arm64/* ./
+rm -rf ./arm64
+
+cd $ZINT_DEV_FOLDER
+
+yarn testfullbuilder:applesilicon
